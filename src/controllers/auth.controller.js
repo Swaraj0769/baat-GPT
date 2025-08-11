895ff38cb2ba33868f1df74255a4ec861a75b8a5
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-08-11 21:23:26 +0530

    Implement user existence check in post registration controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,7 +7,18 @@ async function getRegisterController(req, res) {
 async function postRegisterController(req, res) {
     const {username, email, password} = req.body;
 
-    
+    const isUserExist = await userModel.findOne({
+        $or:[
+            {username: username},
+            {email: email}
+        ]
+    })
+
+    if(isUserExist){
+        return res.status(400).json({
+            message: "User already exists with this username or email"
+        })
+    }
 } 
 
 
