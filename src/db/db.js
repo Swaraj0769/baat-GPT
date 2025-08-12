76caf59736f09b5990839b64f84a0b1ac9064360
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-08-12 17:53:31 +0530

    Add database connection logic: implement connectDB function to establish MongoDB connection

---
+++ src/db/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+function connectDB(){
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(()=> console.log("MongoDB connected"))
+    .catch((err) => console.log('MongoDB connectionn error:', err))
+}
+
+module.exports = connectDB
